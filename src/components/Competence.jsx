a590f1e4e070d5502516b52640a62298b59f1baf
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-08-07 22:15:18 +0300

    sss

--- src/components/Competence.jsx
+++ src/components/Competence.jsx
@@ -9,35 +9,44 @@ import {
 } from 'react-icons/si';
 import '../assets/Competence.css';
 
+const technologies = [
+  { icon: <FaHtml5 />, name: "HTML5" },
+  { icon: <FaCss3Alt />, name: "CSS3" },
+  { icon: <SiJavascript />, name: "JavaScript" },
+  // { icon: <SiTypescript />, name: "TypeScript" },
+  { icon: <FaReact />, name: "React" },
+  // { icon: <SiNextdotjs />, name: "Next.js" },
+  { icon: <SiRedux />, name: "Redux" },
+  { icon: <SiVite />, name: "Vite" },
+  { icon: <SiTailwindcss />, name: "Tailwind CSS" },
+  { icon: <SiBootstrap />, name: "Bootstrap" },
+  { icon: <FaNodeJs />, name: "Node.js" },
+  { icon: <FaPython />, name: "Python" },
+  { icon: <SiDjango />, name: "Django" },
+  // { icon: <SiFlask />, name: "Flask" },
+  { icon: <FaJava />, name: "Java" },
+  { icon: <SiSpringboot />, name: "Spring Boot" },
+  { icon: <SiPostgresql />, name: "PostgreSQL" },
+  { icon: <SiMysql />, name: "MySQL" },
+  { icon: <SiMongodb />, name: "MongoDB" },
+  { icon: <SiFirebase />, name: "Firebase" },
+  { icon: <FaDocker />, name: "Docker" },
+  { icon: <FaGitAlt />, name: "Git" },
+  { icon: <SiLinux />, name: "Linux" },
+];
+
 export default function Competence() {
   return (
     <section id="competence">
       {/* <h2 className="section-title">Skills</h2> */}
       <ul className="icon-list">
-        <li className="icon-item"><FaHtml5 title="HTML5" /></li>
-        <li className="icon-item"><FaCss3Alt title="CSS3" /></li>
-        <li className="icon-item"><SiJavascript title="JavaScript" /></li>
-        {/* <li className="icon-item"><SiTypescript title="TypeScript" /></li> */}
-        <li className="icon-item"><FaReact title="React" /></li>
-        {/* <li className="icon-item"><SiNextdotjs title="Next.js" /></li> */}
-        <li className="icon-item"><SiRedux title="Redux" /></li>
-        <li className="icon-item"><SiVite title="Vite" /></li>
-        <li className="icon-item"><SiTailwindcss title="Tailwind CSS" /></li>
-        <li className="icon-item"><SiBootstrap title="Bootstrap" /></li>
-        <li className="icon-item"><FaNodeJs title="Node.js" /></li>
-        <li className="icon-item"><FaPython title="Python" /></li>
-        <li className="icon-item"><SiDjango title="Django" /></li>
-        {/* <li className="icon-item"><SiFlask title="Flask" /></li> */}
-        <li className="icon-item"><FaJava title="Java" /></li>
-        <li className="icon-item"><SiSpringboot title="Spring Boot" /></li>
-        <li className="icon-item"><SiPostgresql title="PostgreSQL" /></li>
-        <li className="icon-item"><SiMysql title="MySQL" /></li>
-        <li className="icon-item"><SiMongodb title="MongoDB" /></li>
-        <li className="icon-item"><SiFirebase title="Firebase" /></li>
-        <li className="icon-item"><FaDocker title="Docker" /></li>
-        <li className="icon-item"><FaGitAlt title="Git" /></li>
-        <li className="icon-item"><SiLinux title="Linux" /></li>
-       
+        {technologies.map(({ icon, name }, index) => (
+          <li className="icon-item" key={index}>
+            <span className="tooltip">{name}</span>
+            {icon}
+            
+          </li>
+        ))}
       </ul>
     </section>
   );
